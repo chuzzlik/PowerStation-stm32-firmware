1c41f10ac907e70433f6dbfc36c23202707be874
chuzzlik <chuzzlik@gmail.com>
2026-07-20 20:10:01 +0300

    Turn off onboard RGB LED at startup

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,14 @@
 #include <Arduino.h>
 #include "AppController.h"
 
+#ifndef RGB_BUILTIN
+#define RGB_BUILTIN 48
+#endif
+
 AppController app;
 
 void setup() {
+    neopixelWrite(RGB_BUILTIN, 0, 0, 0);
     app.begin();
 }
 
